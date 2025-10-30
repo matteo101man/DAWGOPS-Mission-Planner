d1625562997113d11cc67f65221365ea8de0865b
matteo101man <matteo.garza01@gmail.com>
2025-10-30 12:52:54 -0400

    PWA + iPhone fullscreen; touch tap-to-place for symbols

---
+++ service-worker.js
@@ -0,0 +1,34 @@
+const CACHE_NAME = 'planner-cache-v1';
+const ASSETS = [
+  './',
+  './app.html',
+  './index.html',
+  './manifest.webmanifest',
+  './public/js/app-core.js',
+  './symbols/index.json'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS)));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) => Promise.all(keys.map((k) => k !== CACHE_NAME && caches.delete(k))))
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', (event) => {
+  const req = event.request;
+  event.respondWith(
+    caches.match(req).then((cached) => cached || fetch(req).then((res) => {
+      const copy = res.clone();
+      caches.open(CACHE_NAME).then((cache) => cache.put(req, copy));
+      return res;
+    }).catch(() => cached))
+  );
+});
+
+
